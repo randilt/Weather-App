86b61d47ba34828e079b7d36f85f96e79632001c
RandilT <randiltharusha72@gmail.com>
2023-12-16 08:58:16 +0530

    Import icons to WeatherAPp component

--- src/components/weatherApp/WeatherApp.jsx
+++ src/components/weatherApp/WeatherApp.jsx
@@ -1,4 +1,12 @@
 import "./WeatherApp.css";
+import searchIcon from "../assets/search.png";
+import cloudIcon from "../assets/cloud.png";
+import rainIcon from "../assets/rain.png";
+import drizzleIcon from "../assets/drizzle.png";
+import snowIcon from "../assets/snow.png";
+import humidityIcon from "../assets/humidity.png";
+import windIcon from "../assets/wind.png";
+import clearIcon from "../assets/clear.png";
 
 const WeatherApp = () => {
   return <div>WeatherApp</div>;
