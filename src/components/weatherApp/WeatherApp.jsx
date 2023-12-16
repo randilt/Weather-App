b3f145cf0802532bb2291a15b2ba3645f2c56a37
RandilT <randiltharusha72@gmail.com>
2023-12-16 10:08:55 +0530

    Add functionality using openweathermap api

--- src/components/weatherApp/WeatherApp.jsx
+++ src/components/weatherApp/WeatherApp.jsx
@@ -9,11 +9,48 @@ import windIcon from "../assets/wind.png";
 import clearIcon from "../assets/clear.png";
 
 const WeatherApp = () => {
+  const apiKey = "b89dfd32e827f585f1b6c431bcf92e9e";
+  const search = async () => {
+    const element = document.getElementsByClassName("cityInput");
+    if (element[0].value === "") {
+      return 0;
+    }
+    let url = `https://api.openweathermap.org/data/2.5/weather?q=${element[0].value}&units=Metric&appid=${apiKey}`;
+
+    let res = await fetch(url);
+    let data = await res.json();
+    const humidity = document.getElementsByClassName("humidity-percentage");
+    const wind = document.getElementsByClassName("wind-speed");
+    const temperature = document.getElementsByClassName("weather-temp");
+    const location = document.getElementsByClassName("weather-location");
+    const weatherImage = document.getElementsByClassName("weather-image");
+
+    humidity[0].innerHTML = data.main.humidity + "%";
+    wind[0].innerHTML = data.wind.speed + " km/h";
+    temperature[0].innerHTML = data.main.temp + "&deg;C";
+    location[0].innerHTML = data.name + ", " + data.sys.country;
+    if (data.weather[0].main === "Clouds") {
+      weatherImage[0].innerHTML = `<img src=${cloudIcon} alt="" />`;
+    } else if (data.weather[0].main === "Rain") {
+      weatherImage[0].innerHTML = `<img src=${rainIcon} alt="" />`;
+    } else if (data.weather[0].main === "Drizzle") {
+      weatherImage[0].innerHTML = `<img src=${drizzleIcon} alt="" />`;
+    } else if (data.weather[0].main === "Snow") {
+      weatherImage[0].innerHTML = `<img src=${snowIcon} alt="" />`;
+    } else if (data.weather[0].main === "Clear") {
+      weatherImage[0].innerHTML = `<img src=${clearIcon} alt="" />`;
+    }
+  };
   return (
     <div className="container">
       <div className="top-bar">
         <input type="text" className="cityInput" placeholder="Search" />
-        <div className="search-icon">
+        <div
+          className="search-icon"
+          onClick={() => {
+            search();
+          }}
+        >
           <img src={searchIcon} alt="" />
         </div>
       </div>
@@ -33,7 +70,7 @@ const WeatherApp = () => {
         <div className="element">
           <img src={windIcon} alt="" className="icon" />
           <div className="data">
-            <div className="humidity-percentage">18 km/h</div>
+            <div className="wind-speed">18 km/h</div>
             <div className="text">Wind Speed</div>
           </div>
         </div>
